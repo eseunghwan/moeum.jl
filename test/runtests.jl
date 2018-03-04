35e31ea9c5cb6d3a669a22b6d54a3144214aa0c6
eseunghwan <reignstarbits@gmail.com>
2018-03-04 20:16:59 +0900

    fixed runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using moeum
 using Base.Test
 
 #default test
-@test mo_a = moeum.MOEUM(dat_sori = ["dat1", "dat2"], hol_sori = [[1, 2], [3, 4]], name = "mo_a")
-@test dat1 = mo_a.select("dat1")
-@test dat1.describe()
+mo_a = moeum.MOEUM(dat_sori = ["dat1", "dat2"], hol_sori = [[1, 2], [3, 4]], name = "mo_a")
+dat1 = mo_a.select("dat1")
+dat1.describe()
 
-@test mo_a = moeum.from_csv("mo_a.csv")
-@test dat1 = mo_a.select("dat1")
-@test dat1.describe()
+mo_a = moeum.from_csv("mo_a.csv")
+dat1 = mo_a.select("dat1")
+dat1.describe()
 
-@test mo_a.where("dat1 >= 2")
+mo_a.where("dat1 >= 2")
